03029ae91f22c9f029e4ec31cd4944d2038f7b97
hunlyfriendly@gmail.com <hunly@hunly.local>
2019-02-22 23:08:46 +0700

    Posting with Postman

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+/* Handling all the parsing */
+app.use(bodyParser.json());
 
 app.post('/', (req,res) => {
   var email = req.body.email;
   var amount = req.body.amount;
 
-  res.send({"email" : email, "amount" : amount})
+  res.send({'amount' : amount, 'email' : email})
 });
 
 app.listen(3000,()=> {
