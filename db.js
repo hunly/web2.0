7343443be91e7478c8d879f78467a769a0537089
hunlyfriendly@gmail.com <hunly@hunly.local>
2019-02-23 10:25:18 +0700

    Writing your first query

--- db.js
+++ db.js
@@ -1,24 +1,23 @@
-var msql = require('mysql');
+var mysql = require('mysql');
 var db_config = {
   host : '127.0.0.1',
   user : 'root',
-  password : 'crypodia123',
+  password : 'root@008',
   database : 'webapp'
 }
 
 var connection;
 
 function handDisconnect() {
-  connection = mqsql.createConnetion(db_config);
+  connection = mysql.createConnection(db_config);
   connection.connect(function(err){
-
     if (err) {
       console.log('Error when connecting to db: ', err);
       setTimeout(handDisconnect,2000);
     }
   });
 
-  connction.on('error',function (err) {
+  connection.on('error',function (err) {
     if (err.code==='PROTOCOL_CONNECTION_LOST') {
       handDisconnect();
     }else {
